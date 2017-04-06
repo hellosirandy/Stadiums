f8616c26d88dee490e68d49313145894b3584e37
hellosirandy <sac840711@gmail.com>
2017-04-06 13:15:42 +0800

    fix target center

--- seeds/NBA/target_center.js
+++ seeds/NBA/target_center.js
@@ -10,10 +10,10 @@ var stadium = {
     architect: 'KMR Architects, Ltd.',
     location: 'Minneapolis, Minnesota',
     images: [
+      'https://static1.squarespace.com/static/577585e49f7456a22f26604e/57ae1c79e6f2e1c9bc47fdaa/57ae1e3815d5db42067f09ac/1471028795067/PI-NBA-Target-Center-043014.jpg?format=2500w',
       'http://www.greenroofs.com/projects/target_center/target_center9.gif',
       'https://static.rukkus.com/venue/images/Target_Center-667.jpg',
       'https://shortstorysnapshots.files.wordpress.com/2011/02/imag0654.jpg',
-      'https://static1.squarespace.com/static/577585e49f7456a22f26604e/57ae1c79e6f2e1c9bc47fdaa/57ae1e3815d5db42067f09ac/1471028795067/PI-NBA-Target-Center-043014.jpg?format=2500w',
       'https://cbsminnesota.files.wordpress.com/2017/03/target-center.jpg?w=1200',
       'http://targetcenterrenovation.com/setup/wp-content/uploads/2014/11/tcr-gallery-05.jpg'
     ]
