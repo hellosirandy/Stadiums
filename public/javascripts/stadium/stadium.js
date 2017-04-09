86f4f1d11fb4263db8b8ca48a94c6097a55523ca
hellosirandy <sac840711@gmail.com>
2017-04-09 22:26:59 +0800

    adding modal

--- public/javascripts/stadium/stadium.js
+++ public/javascripts/stadium/stadium.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-  
+  var quill = new Quill('#editor', {
+    theme: 'snow'
+  });
+  $('.modal').modal();
+  $('.storyContent').click(function() {
+    console.log($(this).find('.storyTitle').html());
+    var title = $(this).find('.storyTitle').html();
+    $('#storyModal').find('h4').html(title);
+    $('#storyModal').modal('open');
+  });
 });
