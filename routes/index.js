1242410f16d97dc3022ff94e3b24f1d2a260b269
hellosirandy <sac840711@gmail.com>
2017-04-14 01:03:25 +0800

    fix bug

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 var RouteBasics = require('../middlewares/route-basics');
 
-router.get('/', RouteBasics, function(req, res, next) {
-  res.redirect('/stadium')
+router.get('/', RouteBasics, function (req, res, next) {
+  res.redirect('/stadium');
 });
 
-router.get('/signout', function(req, res) {
+router.get('/signout', function (req, res) {
   req.logout();
   res.redirect(req.get('referer'));
 });
