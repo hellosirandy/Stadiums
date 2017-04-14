e614c0e1c94fad1b185a2fe080b170ab8e3fe32d
Shaoan Chien <andy@ubuntu-1gb-sfo2-01.localdomain>
2017-04-14 18:33:54 +0000

    git forget auth

--- configs/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  'facebookAuth' : {
-    'clientID': '202206926947603',
-    'clientSecret': '74ca3cf9df0a7da717c44015ddb8e0d4',
-    'callbackURL': 'http://ballonhouse.com:3000/auth/facebook/callback'
-  }
-};
