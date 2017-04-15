ead892458e3743eb403e6ab97eabe8e8a3d02686
hellosirandy <sac840711@gmail.com>
2017-04-15 11:49:32 +0800

    'test'

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 router.get('/facebook', passport.authenticate('facebook', {scope: ['public_profile','email']}));
 
-router.get('/facebook/callback', passport.authenticate('facebook', {failureRedirect: '/' }), function(req, res) {
+router.get('/facebook/callback', passport.authenticate('facebook', {successRedirect: '/', failureRedirect: '/' }), function(req, res) {
   console.log('auth');
   console.log(req.user);
   res.redirect(req.get('referer'));
