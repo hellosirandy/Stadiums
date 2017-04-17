1f9c458676ea168732fdd8546ca8592de30ff937
hellosirandy <sac840711@gmail.com>
2017-04-17 10:21:32 +0800

    'test_search'

--- public/javascripts/partials/nav-bar.js
+++ public/javascripts/partials/nav-bar.js
@@ -4,23 +4,23 @@ $(document).ready(function(){
   $('#profilePic').click(function() {
     $(this).dropdown('open');
   });
-//	var allStadiums = JSON.parse($('#allStadiums').html());
-//	$('#searchInput').keyup(function() {
-//		$('#searchResults').html('');
-//		var val = $(this).val();
-//		if (val != '') {
-//			var results = [];
-//			for (var i = 0; i < allStadiums.length; i++) {
-//				if (allStadiums[i].name.toLowerCase().search(val.toLowerCase()) > -1) {
-//					results.push(allStadiums[i]);
-//				}
-//			}
-//			ShowSearchResults(results);
-//		}
-//		else {
-//			HideSearchResults();
-//		}
-//	});
+	var allStadiums = JSON.parse($('#allStadiums').html());
+	$('#searchInput').keyup(function() {
+		$('#searchResults').html('');
+		var val = $(this).val();
+		if (val != '') {
+			var results = [];
+			for (var i = 0; i < allStadiums.length; i++) {
+				if (allStadiums[i].name.toLowerCase().search(val.toLowerCase()) > -1) {
+					results.push(allStadiums[i]);
+				}
+			}
+			ShowSearchResults(results);
+		}
+		else {
+			HideSearchResults();
+		}
+	});
 //	var con = parseFloat($('#navBar .container').width()) - 40;
 //	$('#searchInput').focus(function() {
 //		$('#profilePic').css({'display': 'none'});
