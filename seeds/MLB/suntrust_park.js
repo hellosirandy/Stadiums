f1948a105ef41f9a7b697f73425aa23703269f38
hellosirandy <sac840711@gmail.com>
2017-05-22 01:34:49 +0800

    update Suntrust Park

--- seeds/MLB/suntrust_park.js
+++ seeds/MLB/suntrust_park.js
@@ -10,8 +10,12 @@ var stadium = {
     architect: 'Populous',
     location: 'Atlanta, Georgia',
     images: [
-      'http://atlanta.braves.mlb.com/atl/ballpark/suntrust-park/images/ballpark-rendering2.jpg',
-      'https://pbs.twimg.com/media/C6RdQsZWAAAwFKO.jpg',
+      'http://www.ballparksofbaseball.com/wp-content/uploads/2016/04/suntrust17954.jpg',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/SunTrust_Park_Opening_Day_2017.jpg/1200px-SunTrust_Park_Opening_Day_2017.jpg',
+      'http://www.ballparksofbaseball.com/wp-content/uploads/2016/04/suntrust17950-1.jpg',
+      'https://www.gannett-cdn.com/-mm-/95a42ab969f59cc51e0631858f3d50fc2b79f50d/c=326-0-5433-3840&r=x1443&c=1920x1440/local/-/media/2017/03/31/USATODAY/USATODAY/636265566295555127-AP-Braves-Game-Changing-Complex-Baseball-89884288-670937.JPG',
+      'https://cdn1.vox-cdn.com/uploads/chorus_asset/file/8247631/braves_chophouse_suntrust_park.jpg',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/SunTrust_Park_view_from_right_field%2C_May_2017.jpg/1280px-SunTrust_Park_view_from_right_field%2C_May_2017.jpg',
       
     ]
   }
