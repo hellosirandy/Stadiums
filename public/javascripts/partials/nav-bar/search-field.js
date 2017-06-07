57de2b771c90817be27f9d022632e8c4f4b11f2e
hellosirandy <sac840711@gmail.com>
2017-06-07 21:52:59 +0800

    working arrow selection

--- public/javascripts/partials/nav-bar/search-field.js
+++ public/javascripts/partials/nav-bar/search-field.js
@@ -1,34 +1,79 @@
 $(document).ready(function() {
-  var allStadiums = $('#search-result').data('stadiums')
-  $('#search-input').focus(function() {
-    $('#account-li').hide();
+  up = 38;
+  down = 40;
+  searchResultHighlight = -1;
+  var allStadiums = $('#search-result').data('stadiums');
+  $('#search-wrapper').click(function() {
+    if (!$('#search-input').hasClass('focus')) {
+      $('#account-li').hide();
+      $('#search-input').addClass('focus');
+      val = $('#search-input').val();
+      search(val, allStadiums);
+    }
   });
-  $('#search-input').focusout(function() {
-    $('#account-li').show();
-    // $('#search-result').hide();
-  });
-  $('#search-input').keyup(function() {
+  var selecting = false;
+  var $cur;
+  $('#search-input').keyup(function(e) {
     $('#search-result ul').html('');
 		var val = $(this).val();
-		if (val != '') {
-			for (var i = 0; i < allStadiums.length; i++) {
-				if (allStadiums[i].name.toLowerCase().search(val.toLowerCase()) > -1) {
-          $('#search-result ul').append(
-            `<a href="${allStadiums[i].url}" class="collection-item">${allStadiums[i].name}</a>`
-      		);
-				}
-			}
-      $('#search-result').show();
-		}
-		else {
-      $('#search-result').hide();
-		}
+    search(val, allStadiums);
+    if ($('#search-result').is(':visible')) {
+      selecting = true;
+      if (!selecting) {
+        $cur = $('#search-result a').first();
+      }
+      $cur.toggleClass('grey lighten-2');
+      switch (e.which) {
+        case down:
+          console.log($cur.html());
+          // $cur.toggleClass('grey lighten-2');
+          $cur = $cur.next();
+          // $cur.toggleClass('grey lighten-2');
+          // console.log($('#search-result a').first().html());
+          // searchResultHighlight += 1;
+          // $('#search-result a').eq(searchResultHighlight).toggleClass('grey lighten-2');
+          break;
+        case up:
+          console.log($cur.html());
+          $cur.toggleClass('grey lighten-2');
+          $cur = $cur.prev();
+          $cur.toggleClass('grey lighten-2');
+          break
+        default:
+          return;
+      }
+      e.preventDefault();
+    }
   });
-  // $(document).mouseup(function(e) {
-  //   var trigger = $('#search-input');
-  //   var container = $('#search-result');
-  //   if (!trigger.is(e.target) && !container.is(e.target) && trigger.has(e.target).length === 0){
-  //       container.hide();
-  //   }
+  // $('#search-result ul a').hover(function() {
+  //   $('#search-result a').eq(searchResultHighlight).toggleClass('grey lighten-2');
+  //   searchResultHighlight =
   // });
+  $(document).mouseup(function(e) {
+    var trigger = $('#search-input');
+    var container = $('#search-wrapper');
+    if (container.has(e.target).length === 0) {
+      if ($('#search-input').hasClass('focus')) {
+        $('#account-li').show();
+        $('#search-input').removeClass('focus');
+        $('#search-result').hide();
+      }
+    }
+  });
 });
+
+function search(inputText, allStadiums) {
+  if (inputText != '') {
+    for (var i = 0; i < allStadiums.length; i++) {
+      if (allStadiums[i].name.toLowerCase().search(inputText.toLowerCase()) > -1) {
+        $('#search-result ul').append(
+          `<a href="${allStadiums[i].url}" class="collection-item">${allStadiums[i].name}</a>`
+        );
+      }
+    }
+    $('#search-result').show();
+  }
+  else {
+    $('#search-result').hide();
+  }
+}
